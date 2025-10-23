1c5b30a3640ca6b79eb794be13b7edad11aaca55
gajanand <gajanand2005singh@gmail.com>
2025-10-23 20:49:30 +0530

    commit is done

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO: Increase Image Size in AdsBannerSlider
-
-- [x] Edit src/components/BannerBox/index.jsx to add height class (h-64) to the img element for larger images.
--- src/components/BannerBox/index.jsx
+++ src/components/BannerBox/index.jsx
@@ -5,7 +5,7 @@ const BannerBox  = (props) => {
     <div>
        <div className='box bannerBox overflow-hidden rounded-lg group'>
         <Link to={props.link || '/'}>
-            <img src={props.img} alt="banner" className='w-full h-64 transition-all group-hover:scale-105 group-hover:rotate-2' />
+            <img src={props.img} alt="banner" className='w-[100%] !h-[200px] transition-all group-hover:scale-105 group-hover:rotate-2' />
         </Link>
             </div>
            
